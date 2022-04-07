d3634046b54001a4ea3dc87e33616e5481777d6d
Whitney Desbonnes <whitney.desbonnes@gmail.com>
2022-04-07 20:13:44 +0200

    partie js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
 let rating = document.querySelector('.rating');
 let thankyou = document.querySelector('.thankyou');
 let button = document.querySelector('button');
-console.log(button);
+
 
 button.addEventListener('click', () => {
     thankyou.classList.remove('switch');
     rating.classList.add('switch');
 })
+
+let ratingNumbers = document.querySelectorAll('.rating__numbers div');
+let insertText = document.querySelector('.insertText');
+
+
+
+ratingNumbers.forEach (item => {
+    item.addEventListener('click', () => {
+
+        for(i=0; i < ratingNumbers.length; i++) {
+            ratingNumbers[i].classList.remove('btnActive');
+        }
+
+        item.classList.toggle('btnActive');
+        insertText.textContent = item.dataset.numbers;
+
+    })
+})
+
